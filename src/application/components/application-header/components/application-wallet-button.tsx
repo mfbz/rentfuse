a63e9ea3414a11616c3dbadbc9861c58be39164b
mfbz <michael.fabozzi@icloud.com>
2021-06-20 12:04:47 +0200

    Added icon wallet button on mobile

--- src/application/components/application-header/components/application-wallet-button.tsx
+++ src/application/components/application-header/components/application-wallet-button.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Button } from 'antd';
 import { useResponsive } from '../../../../common/hooks/use-responsive';
 import Icon from '@ant-design/icons';
+import { WalletIcon } from '../../../../common/icons/wallet-icon';
 
 export const ApplicationWalletButton = React.memo(function ApplicationWalletButton({}: {}) {
 	const { isMobileAndBelow } = useResponsive();
 
 	return (
 		<div>
-			<Button type={'primary'} size={'large'} shape={'round'}>
-				{'Connect wallet'}
-			</Button>
+			{isMobileAndBelow ? (
+				<Button type={'primary'} size={'large'} shape={'circle'} icon={<Icon component={WalletIcon} />} />
+			) : (
+				<Button type={'primary'} size={'large'} shape={'round'}>
+					{'Connect wallet'}
+				</Button>
+			)}
 
 			<style jsx>{``}</style>
 		</div>

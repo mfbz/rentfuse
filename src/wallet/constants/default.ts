b2d8f3097b5dba483eedbe6f85c70c0784d4f18c
mfbz <michael.fabozzi@icloud.com>
2021-07-05 23:03:05 +0200

    Updated default.ts file to connect to testnet

--- src/wallet/constants/default.ts
+++ src/wallet/constants/default.ts
@@ -9,16 +9,20 @@ export const DEFAULT_LOGGER = 'debug';
 export const DEFAULT_METHODS = ['invokefunction'];
 export const DEFAULT_RELAY_PROVIDER = 'wss://connect.coz.io:443';
 
-//export const DEFAULT_GAS_SCRIPTHASH = '0xd2a4cff31913016155e38e474a2c06d08be276cf';
-//export const DEFAULT_GAS_PRECISION = 10 ** 8;
+// TESTNET
+export const DEFAULT_GAS_SCRIPTHASH = '0xd2a4cff31913016155e38e474a2c06d08be276cf';
+export const DEFAULT_GAS_PRECISION = 10 ** 8;
 
-//export const DEFAULT_NEO_RPC_ADDRESS = 'https://testnet1.neo.coz.io:443';
-//export const DEFAULT_NEO_NETWORK_MAGIC = 844378958;
-//export const DEFAULT_SC_SCRIPTHASH = '0x74af22c6b7304251eeebfd12ec53f82a40495b50';
+export const DEFAULT_NEO_RPC_ADDRESS = 'https://testnet1.neo.coz.io:443';
+export const DEFAULT_NEO_NETWORK_MAGIC = 844378958;
+export const DEFAULT_SC_SCRIPTHASH = '0x74af22c6b7304251eeebfd12ec53f82a40495b50';
 
+// LOCALNET
+/*
 export const DEFAULT_GAS_SCRIPTHASH = '0xd2a4cff31913016155e38e474a2c06d08be276cf';
 export const DEFAULT_GAS_PRECISION = 10 ** 8;
 
 export const DEFAULT_NEO_RPC_ADDRESS = 'http://127.0.0.1:50012';
 export const DEFAULT_NEO_NETWORK_MAGIC = 943915546;
 export const DEFAULT_SC_SCRIPTHASH = '0x063fcad8cb5015465365dd63af52d000bbbc4d44';
+*/
